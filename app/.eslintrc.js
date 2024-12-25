4e103e5a3b5e861e135b76c6569393ac8f6b54c6
luqas11 <lucascazas@hotmail.com>
2024-12-25 17:52:59 -0300

    Upgrade React Native version from 0.71 to 0.76

---
+++ app/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native', 'plugin:jsdoc/recommended-typescript'],
+  plugins: ['jsdoc'],
+};
--- bombitapp/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@react-native-community', 'plugin:jsdoc/recommended-typescript'],
-  plugins: ['jsdoc'],
-  rules: {'prettier/prettier': ['error', {endOfLine: 'auto'}]},
-};
